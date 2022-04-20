b5bdebeec6bfa015dc1a99d7a5bc936de3b0009b
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-04-20 13:00:33 +0530

    hollywodd data update

--- src/Data.js
+++ src/Data.js
@@ -10,27 +10,34 @@ export default [
             title: "Jurassic World Dominion",
             releaseDate: " 10 June 2022 ",
             imgUrl: "https://m.media-amazon.com/images/M/MV5BMjQzZTA2MmEtNDcwYy00ZWE0LWFlODMtOWQ2MzRjNTgyYmVkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-        description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
+            description: "The future of mankind hangs in the balance as humans and dinosaurs coexist following the destruction of Isla Nublar.",
             trailer: "https://youtu.be/aWzlQ2N6qqg"
+    }, {
+            title: "Lightyear",
+            releaseDate: " 17 June 2022 ",
+            imgUrl: "https://lumiere-a.akamaihd.net/v1/images/p_lightyear_v2_22043_60ba0a61.jpeg?region=0%2C0%2C540%2C810",
+            description: "The origin story of action figure Buzz Lightyear and his adventures to infinity and beyond.",
+        trailer: "https://youtu.be/aWzlQ2N6qqg"
     },
-    {
-        title: "Jurassic World Dominion",
-        releaseDate: " 10 June 2022 ",
-        imgUrl: "https://m.media-amazon.com/images/M/MV5BMjQzZTA2MmEtNDcwYy00ZWE0LWFlODMtOWQ2MzRjNTgyYmVkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-        description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
-       trailer: "https://youtu.be/aWzlQ2N6qqg"
-    },    {
+       {
         title: "Thor Love And Thunder",
         releaseDate: " 8 July 2022 ",
-        imgUrl: "https://m.media-amazon.com/images/M/MV5BMjQzZTA2MmEtNDcwYy00ZWE0LWFlODMtOWQ2MzRjNTgyYmVkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
-        description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
+        imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Thor_Love_and_Thunder_poster.jpeg/220px-Thor_Love_and_Thunder_poster.jpeg",
+        description: "Thor: Love and Thunder is an upcoming American superhero film based on the Marvel Comics character Thor, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures. It is intended to be the direct sequel to Thor: Ragnarok and the 29th film in the Marvel Cinematic Universe",
        trailer: "https://youtu.be/aWzlQ2N6qqg"
     },
+    {
+        title: "DC League of Super-Pets",
+        releaseDate: " 29 July 2022 ",
+        imgUrl: "https://i.ytimg.com/vi/ME_upzi4boc/maxresdefault.jpg",
+        description: "Krypto the Super-Dog and Superman are inseparable best friends, sharing the same superpowers and fighting crime side by side in Metropolis. However, when the Man of Steel and the rest of the Justice League are kidnapped, Krypto must convince a ragtag group of animals to master their own newfound powers for a rescue mission.",
+        trailer: "https://youtu.be/aWzlQ2N6qqg"
+    }, 
     {
         title: "Black Adam",
         releaseDate: "21 Oct 2022",
         imgUrl: "https://m.media-amazon.com/images/M/MV5BZDg5ZDg2MWQtM2ExNi00ZjEzLTgzMDQtZmJlYWEwYmM4ODUxXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
-        description: "This is a sequel to Dr Strange released in 2016 which has high expections among fans all over the world.",
+        description: "Black Adam is an upcoming American superhero film based on the DC Comics character of the same name. Produced by New Line Cinema, DC Films, Seven Bucks Productions, and FlynnPictureCo., and set for distribution by Warner Bros",
         trailer: "https://youtu.be/aWzlQ2N6qqg"
     },
     {
